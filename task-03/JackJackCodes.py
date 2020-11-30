f33c2c79fde2d7405744b1f64d8d4afd0ef2da62
Manda Snehashish Reddy <ashishreddy9000@gmail.com>
2020-11-30 14:53:56 +0530

    Finally found a solution

--- task-03/JackJackCodes.py
+++ task-03/JackJackCodes.py
@@ -1,15 +1,17 @@
 n,m = map(int, input().split())
-array = list(map(int, input().split()))
-identifier = 1
-for x in range(0, n-1):
-    for y in range(x+1, n):
-        if (array[x] + array[y] == m):
-            identifier = 1
-            print(True)
-            break
-        else:
-            identifier = 0
-    if identifier == 1:
+arr = list(map(int, input().split()))
+arr.sort()
+x = 0
+y = n-1
+identifier = 0
+while x < y:
+    if arr[x] + arr[y] == m:
+        print(True)
+        identifier = 1
         break
+    elif arr[x] + arr[y] < m:
+        x+=1
+    else:
+        y-=1
 if identifier == 0:
     print(False)
